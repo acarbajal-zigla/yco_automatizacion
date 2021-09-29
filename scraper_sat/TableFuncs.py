289ec3966532d10f33c1c9fe76bb5c6f90a5e752
acarbajal-zigla <acarbajal@ziglaconsultores.com>
2021-09-29 16:54:48 -0300

    Obtencion de tablas funcionando - TODO: revisar por errores en caso de no tener datos

--- scraper_sat/TableFuncs.py
+++ scraper_sat/TableFuncs.py
@@ -4,26 +4,26 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from constantes import HEAD
 
-def get_headers_tabla(tabla:webdriver.WebElement):
-    headers = [element.gett_attribute('innerHTML') for element in tabla.find_elements_by_class_name('tablaHeader')] 
+
+def get_headers_tabla(tabla):
+    headers = [element.get_attribute('innerHTML') for element in tabla.find_elements_by_class_name('tablaHeader')] 
     return headers
 
-def get_valores_renglones(tabla:webdriver.WebElement):
+def get_valores_renglones(tabla):
     renglones = tabla.find_elements_by_xpath(".//tr[contains(@class,'renglon')]")
     valores=[]
     for renglon in renglones:
-        valores.append(tuple([a for a in renglon.find_elements_by_xpath(".//*") if not(a.get_attribute('innerHTML').startsWith('<'))]))
+        valores.append(tuple([a.get_attribute('innerHTML') for a in renglon.find_elements_by_xpath(".//*") if not(a.get_attribute('innerHTML').startswith('<'))]))
     return valores
 
 def get_datos_tabla(browser: webdriver.Chrome, table_data:dict):
-    # table_data = {'categoria':CAT, 'subcategoria':SUBCAT}
-    HEAD = 'transparenciaDetForm'
+    # diccionario table_data -> {'categoria':CAT, 'subcategoria':SUBCAT}
 
     ids = {'table':f"{HEAD}:id{table_data['categoria']}Registro:dataTable{table_data['subcategoria']}", 
             'scroll':f"{HEAD}:id{table_data['categoria']}Registro:scroll{table_data['subcategoria']}next"
             }
-
     # Encuentro la tabla y la guardo en la variable "tabla"
     wait = WebDriverWait(browser, 10)
     wait.until(EC.presence_of_element_located((By.ID, ids['table']+':tbody_element')))
@@ -32,7 +32,7 @@ def get_datos_tabla(browser: webdriver.Chrome, table_data:dict):
     # Guardo headers de la tabla
     headers = get_headers_tabla(tabla)
     button_next_scroller = browser.find_element_by_id(ids['scroll'])
-    scroller = button_next_scroller.find_element_by_xpath('../..')
+    scroller = button_next_scroller.find_elements_by_xpath('../../td')
     paginas = len(scroller) - 5
 
     valores=[]
@@ -42,9 +42,13 @@ def get_datos_tabla(browser: webdriver.Chrome, table_data:dict):
 
     for page in range(1, paginas + 1):
         wait.until(EC.presence_of_element_located((By.ID, ids['table']+':tbody_element')))
+        tabla = browser.find_element_by_id(ids['table'])
         valores = get_valores_renglones(tabla)
         if paginas > 1 and page < paginas:
-            browser.find_element_by_id(ids['scroll'] + 'next').click()
+            wait.until(EC.presence_of_element_located((By.ID, ids['scroll'])))
+            browser.find_element_by_id(ids['scroll']).click()
+
+
     
     data_tabla = dict()
     for tupla in valores:
--- scraper_sat/scraper_yco.py
+++ scraper_sat/scraper_yco.py
@@ -4,90 +4,43 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-import re
-import get_table_data
-
-NAMES = {
-    # Datos generales
-    'Mision':'transparenciaDetForm:idGralesRegistro:_idJsp31',
-    'Vision':'transparenciaDetForm:idGralesRegistro:_idJsp36',
-    'Pagina web':'transparenciaDetForm:idGralesRegistro:_idJsp41',
-    'Anio de autorizacion':'transparenciaDetForm:_idJsp26',
-    'Socios o Asociados':'transparenciaDetForm:idGralesRegistro:_idJsp67',
-    # Plantillas de personal
-    'Plantilla laboral': 'transparenciaDetForm:idGralesRegistro:_idJsp78',
-    'Plantilla voluntariado': 'transparenciaDetForm:idGralesRegistro:_idJsp79',
-    'Monto total plantilla laboral': 'transparenciaDetForm:idEgresosRegistro:_idJsp166',
-    # Patrimonio
-    'Activo': 'transparenciaDetForm:idIngresosRegistro:_idJsp107',
-    'Pasivo': 'transparenciaDetForm:idIngresosRegistro:_idJsp108',
-    'Capital': 'transparenciaDetForm:idIngresosRegistro:_idJsp109',
-    # Gastos
-    'Gastos administracion': 'transparenciaDetForm:idEgresosRegistro:_idJsp200',
-    'Gastos operacion': 'transparenciaDetForm:idEgresosRegistro:_idJsp201',
-    'Gastos representacion': 'transparenciaDetForm:idEgresosRegistro:_idJsp202'
-}
+import TableFuncs
+from constantes import TABLAS, NAMES
 
 def get_osc_field(browser, dict_osc, field_name):
-    
-    REGEX_WEB = re.compile(r"window\.open\(\'<web>\', \'popupWindowName\', \'menubar=no, toolbar=no, status=yes\'\); return false;")
-    
+    print('Procesando {}'.format(field_name))
     field = browser.find_element_by_name(NAMES[field_name])
+
     if field.tag_name == 'input':
         dict_osc[field_name] = field.get_attribute('value')
     elif field.tag_name == 'textarea':
         dict_osc[field_name] = field.text
     elif field.tag_name == 'a':
-        field = field.get_attribute('href')
-        match = REGEX_WEB.match(field)
-        dict_osc[field_name] = match.group('web')
+        dict_osc[field_name] = field.get_attribute('innerHTML')
 
 # Funcion que scrapea todos los datos devueltos por la consulta
 def get_osc_data(browser):
-    osc = {}
+    osc = dict()
 
     for key, value in NAMES.items():
         get_osc_field(browser, osc, key)
     
     # Rubros autorizados
-    rubro_aut = browser.find_element_by_id("transparenciaDetForm:idGralesRegistro:idRubro2")
+    rubro_aut = browser.find_element_by_id(NAMES['Rubros autorizados'])
     rubro_aut = Select(rubro_aut)
     rubros = [opt.text for opt in rubro_aut.options]
-    osc["rubros_autorizados"] = rubros
+    osc['rubros_autorizados'] = rubros
 
-# Egresos
-    # Montos y conceptos desarrollo directo de la actividad
-    wait = WebDriverWait(browser, 10)
-    wait.until(EC.presence_of_element_located((By.ID, 'transparenciaDetForm:idEgresosRegistro:dataTableMontosConceptos:tbody_element')))
-    tabla = browser.find_element_by_id("transparenciaDetForm:idEgresosRegistro:dataTableMontosConceptos:tbody_element")
-    montos=[]
-    aux=dict()
-    
-    body = tabla.find_element_by_xpath('../../../../../../../../tr[4]/td')
-    scroller = body.find_elements_by_xpath(".//table[contains(@class,'scroller')]/tbody/tr/td")
-    
-    for page in range(1,len(scroller) - 4):
-        browser.find_element_by_id(f"transparenciaDetForm:idEgresosRegistro:scrollMontosConceptosidx{page}").click()
-        wait.until(EC.presence_of_element_located((By.ID, 'transparenciaDetForm:idEgresosRegistro:dataTableMontosConceptos:tbody_element')))
-        tabla = browser.find_element_by_id("transparenciaDetForm:idEgresosRegistro:dataTableMontosConceptos:tbody_element")
-        for renglon in tabla.find_elements_by_xpath(".//tr[contains(@class,'renglon')]"):
-            for a in renglon.find_elements_by_xpath(".//*"):
-                print(a.get_attribute('innerHTML'))
-            aux={'monto':'', 'concepto':''}
-            aux['monto'] = renglon.find_element_by_xpath(f"./td[1]/table/tbody/tr/td").text
-            aux['concepto'] = renglon.find_element_by_xpath(f"./td[2]").text
-            montos.append(aux)
+    # Tablas
+    for table_data in TABLAS:
+        data = TableFuncs.get_datos_tabla(browser, table_data)
+        print(f"TABLA: {table_data['categoria']} - {table_data['subcategoria']}\n")
+        print(data)
+        print('\n')
+
 # Ejercicio Fiscal
     # Selector --> transparenciaDetForm:idSelectEjercicioFiscal (childs -> anios disponibles)
     # Boton para consulta de anio --> transparenciaDetForm:_idJsp22
-
-# Ingresos
-    # transparenciaDetForm:idIngresosRegistro:dataTableIngresos
-    # |
-    #  -> transparenciaDetForm:idIngresosRegistro:dataTableIngresos:tbody_element
-    # Obtener todos los elementos de la tabla --> tabla_body.find_elements_by_css_selector("*")
-
-    # Total --> class =piePaginaText
 
     return osc
 
