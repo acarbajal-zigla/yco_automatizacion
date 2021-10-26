94ccb885731adee6bfb7f7cd365e2f6b74994da8
Alen <acarbajal@ziglaconsultores.com>
2021-10-26 14:10:33 -0300

    .

--- rda_sat/main.py
+++ rda_sat/main.py
@@ -1,9 +1,49 @@
+from PyPDF2.generic import DictionaryObject
 from ordenar_RDA import get_table_from_pdf
 
-PDF_path = r'C:\Users\ZIGLA\Desktop\RDA_2020.pdf'
 
-for rango in ['8-161', '316-467', '470-600']:
+
+dict_entidades_fed = {"AGUASCALIENTES":"Aguascalientes",
+"BAJA CALIFORNIA":"Baja California",
+"BAJA CALIFORNIA SUR":"Baja California Sur",
+"CAMPECHE":"Campeche",
+"CHIAPAS":"Chiapas",
+"CHIHUAHUA":"Chihuahua",
+"CIUDAD DE MÉXICO":"CDMX",
+"COAHUILA":"Coahuila de Zaragoza",
+"COLIMA":"Colima",
+"DURANGO":"Durango",
+"GUANAJUATO":"Guanajuato",
+"GUERRERO":"Guerrero",
+"HIDALGO":"Hidalgo",
+"JALISCO":"Jalisco",
+"MÉXICO":"México",
+"MICHOACÁN":"Michoacán de Ocampo",
+"MORELOS":"Morelos",
+"NAYARIT":"Nayarit",
+"NUEVO LEÓN":"Nuevo León",
+"OAXACA":"Oaxaca",
+"PUEBLA":"Puebla",
+"QUERÉTARO":"Querétaro",
+"QUINTANA ROO":"Quintana Roo",
+"SAN LUIS POTOSÍ":"San Luis Potosí",
+"SINALOA":"Sinaloa",
+"SONORA":"Sonora",
+"TABASCO":"Tabasco",
+"TAMAULIPAS":"Tamaulipas",
+"TLAXCALA":"Tlaxcala",
+"VERACRUZ":"Veracruz de Ignacio de la Llave",
+"YUCATÁN":"Yucatán",
+"ZACATECAS":"Zacatecas"}
+
+# rangos = {'2014':'7-141', '2015':'6-155', '2016':'6-168', '2017': '6-171'}
+
+rangos_por_anio = {'2021':'8-180'} # ['183-379','383-566']
+
+for anio, rango in rangos_por_anio.items():
+    PDF_path = f"C:/Users/ZIGLA/Desktop/RDA_{anio}.pdf"
     df = get_table_from_pdf(PDF_path, rango)
-    df.to_excel(f"C:/Users/ZIGLA/Desktop/Tabla_{rango}.xlsx", index=False)
-# TODO: reemplazar los nombres de columnas
+    for entidad_RDA, entidad_correcta in dict_entidades_fed.items():
+        df.loc[df["ENTIDAD FEDERATIVA"] == entidad_RDA, "ENTIDAD FEDERATIVA"] = entidad_correcta
+    df.to_excel(f"C:/Users/ZIGLA/Desktop/RDA_excels/{anio}/Tabla_{rango}.xlsx", index=False)
 
--- rda_sat/ordenar_RDA.py
+++ rda_sat/ordenar_RDA.py
@@ -7,14 +7,13 @@ import numpy as np
 
 def get_headers_from_dataframe(df: pd.DataFrame):
     for index, row in df.iterrows():
-        if (row[0] == "RFC") and (row[1] == "DENOMINACIÓN O RAZÓN SOCIAL"):
+        if (row[0] == "RFC") and (row[1] == "DENOMINACION SOCIAL"):
             alto_header = 1
             while(df.iloc[index+alto_header, 0] == "RFC"):
                 alto_header += 1
-
             for column in range(2, len(row.tolist())):
                 for i in range(1, alto_header):
-                    row[column] += f" - {df.iloc[index+i, column]}"
+                    row[column] = f"{row[column]} - {df.iloc[index+i, column]}"
             return [a.replace("\n", "").strip() for a in row.tolist()]
         else:
             continue
@@ -28,6 +27,11 @@ def set_entidades_federativas(df: pd.DataFrame):
     df.loc[:,"ENTIDAD FEDERATIVA"] = df.loc[:,"ENTIDAD FEDERATIVA"].str.replace("TOTAL ", "")
     df["ENTIDAD FEDERATIVA"] = df["ENTIDAD FEDERATIVA"].bfill()
 
+def set_entidades_federativas_viejo(df: pd.DataFrame):
+    df.loc[df["RFC"] == df[df.columns[2]],"ENTIDAD FEDERATIVA"] = df.loc[df["RFC"] == df[df.columns[2]],"RFC"]
+    df["ENTIDAD FEDERATIVA"] = df["ENTIDAD FEDERATIVA"].ffill()
+
+
 def get_header_row(ws:Worksheet):
     """
         Busca la fila en que aparecen los encabezados de la tabla.
@@ -36,6 +40,7 @@ def get_header_row(ws:Worksheet):
     """
     for row in ws.iter_rows():
         if row[0].value == "ENTIDAD FEDERATIVA" or row[0].value == "RFC":
+            print(row[0].row)
             return row[0].row
     return False
 
@@ -76,26 +81,32 @@ def get_tabla_entidades_federativas(ws: Worksheet):
 
 def get_table_from_pdf(path, hojas):
 
-    tables = camelot.read_pdf(path, pages=hojas, copy_text=['h', 'v'],  line_scale=80)
-    
+    tables = camelot.read_pdf(path, pages=hojas, copy_text=['h', 'v'],  line_scale=100)
+
     # Concateno todas las tablas en un dataframe
     df = pd.concat([t.df for t in tables])
+    #df = df[df.columns[:-2]] # en los datos viejos elimina la columna de datos inválidos
 
     # Obtengo y asigno headers
     headers = get_headers_from_dataframe(df)
+    #headers = [header for header in headers if (header.startswith("nan") == False)] # en datos viejos no contabiliza la columna nan-nan
     df.columns = headers
 
     # Elimino las filas que son repetición de headers por cambio de entidad federativa
     df = df[df["RFC"] != "RFC"]
+    df = df[df["RFC"].str.startswith("TOTAL ") == False]
+
     # Elimino todas las filas vacias
     df = df.dropna()
     df = df[df["RFC"].astype(bool)]
 
     # Agrego las columnas de categoria y entidad federativa
-    df[["CATEGORIA", "ENTIDAD FEDERATIVA"]] = np.nan
-        
+    df["CATEGORIA"] = np.nan
+    df["ENTIDAD FEDERATIVA"] = np.nan
     set_categorias(df)
     set_entidades_federativas(df)
-    df = df[df[df.columns[0]] != df["CATEGORIA"]]
+    #set_entidades_federativas_viejo(df)
+
+    df = df[df["RFC"] != df["CATEGORIA"]]
     df = df.replace("-","")
     return df
